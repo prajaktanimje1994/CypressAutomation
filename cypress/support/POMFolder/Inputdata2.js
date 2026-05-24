458a0a3c90c99e2ca78f4cc70d11e793d541c1b9
prajaktanimje1994 <nimjeprajakta1994@gmail.com>
2026-05-24 18:08:43 +0530

    success

---
+++ cypress/support/POMFolder/Inputdata2.js
@@ -0,0 +1,26 @@
+class Inputdata2
+{
+    checkboxes(){
+    cy.get('#male').click()
+    cy.get('#sunday').click()
+    cy.get('#country').select('india')
+    cy.get('#colors').select('green')
+    }
+
+    datepicker(){
+    cy.get('input#datepicker').click()
+    cy.get('table.ui-datepicker-calendar tbody tr td a[data-date="29"]').click()
+    cy.get('a span.ui-icon-circle-triangle-w').dblclick()
+    cy.get('table.ui-datepicker-calendar tbody tr td a[data-date="24"]').click()
+
+    }
+
+    datepicker2()
+    {
+        return cy.get('div#HTML15 h2')
+        
+    }
+
+}
+
+export default Inputdata2;
